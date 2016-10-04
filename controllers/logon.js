185edf89b95ff832fc1681315d7f4b7ff896b784
Lebeau Frédéric <G89494@INT.SYS.SHARED.FORTIS>
2016-10-04 22:33:38 +0200

    logon

--- controllers/logon.js
+++ controllers/logon.js
@@ -13,31 +13,9 @@ var Logger = require('../lib/logger');
  * Private functions
  ************************************/
 
-/**
- * build sign-in-end-point based on environment variables and manage production VS Dev(stubbed environement)
- * @return {signinEndPoint} Full URL targetted by "connect with Strava" button 
- */
-function buildSignInEndPoint (){
-  return process.env.STRAVA_SIGN_IN_END_POINT_URI;
-}
-
 /***********************************
  * rendering functions
  ************************************/
-
-/**
- * render logon screen
- *
- * @param {req} request
- * @param {res} response
- */
-function renderIndex(req,res){
-  res.render('signin', {
-    title: 'Sign in',    
-    signInEndPoint:buildSignInEndPoint(),
-    layout: 'logon'
-  });
-}
 
 /**
  * welcome screen
