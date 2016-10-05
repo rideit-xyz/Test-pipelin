ceb3093221666b4209f3fe25290cdee39f48e5e5
Lebeau Frédéric <G89494@INT.SYS.SHARED.FORTIS>
2016-10-05 17:58:23 +0200

    update to DB

--- controllers/logon.js
+++ controllers/logon.js
@@ -8,11 +8,18 @@
  ************************************/
 var express = require('express');
 var Logger = require('../lib/logger');
+var base = require('airtable').base('appdK77fBnr8jkoUn');
 
 /***********************************
  * Private functions
  ************************************/
+function insertUserInAirtable (user){
+  base('Table 1').create({displayName:user.displayName,strava:JSON.stringify(user)}, function(err, record) {
+    if (err) { console.log(err); return; }
+    console.log(record);
+});
 
+}
 /***********************************
  * rendering functions
  ************************************/
@@ -24,6 +31,7 @@ var Logger = require('../lib/logger');
  * @param {res} response
  */
 function renderWelcome(req,res){
+  insertUserInAirtable(req.user);
   res.render('welcome', {
     title: 'Welcome', user: req.user,json:JSON.stringify(req.user),
     layout: 'single-page'
